b5c649605019b0c36c128af301f761a9e17ebf5e
Dimitris Kaitantzidis <james_k23@hotmail.gr>
2019-08-25 00:19:24 +0300

    Added create gmass cmd and nodejs exec to see output in console at first

--- server/utils/connectToEngine.js
+++ server/utils/connectToEngine.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { exec } = require('child_process');
 
 
 const connectToEngine = (starModels) => {
@@ -12,14 +13,17 @@ const connectToEngine = (starModels) => {
     switch (os) {
         case 'win32':
             const pathToRnsExe = path.join(__dirname, '..', '..', 'resources', 'engine', 'windows', 'rns.exe');
+            let pathToEosFile = path.join(__dirname, '..', '..', 'resources', 'eos-files');
             console.log('Path to rns file:', pathToRnsExe);
             console.log('starModels length:', starModels.length);
             console.log('starModels model:', starModels[0].model);
             
             
-            cmd = pathToRnsExe + ` -f ${starModels[0].eosFile} -t ${starModels[0].model}`;
 
             if(starModels.length === 1){
+                pathToEosFile = path.join(pathToEosFile, starModels[0].eosFile);
+                cmd = pathToRnsExe + ` -f ${pathToEosFile} -t ${starModels[0].model}`;
+
                 switch (starModels[0].model) {
                     case 'model':
                         cmd += ` -e ${starModels[0].centralEnergyDensity}` 
@@ -29,14 +33,31 @@ const connectToEngine = (starModels) => {
                                 + `${starModels[0].limit !== 'none' ? ` -n ${starModels[0].measurements}` : ''}`
                                 + `${starModels[0].readingsIgnored ? ' -d 0' : ''}`
                                 
-                         console.log('cmd for model:', cmd);
-                            
-                        
+                        console.log('cmd for model:', cmd);
+
                         break;
-                
+                    case 'gmass':
+                        cmd += ` -e ${starModels[0].centralEnergyDensity}` 
+                                + `${starModels[0].limit == 'limitEnergy' ? ` -l ${starModels[0].limitValue}` : ''}`
+                                + ` -m ${starModels[0].valueForSecondInput}`
+                                + `${starModels[0].limit == 'limitSecondValue' ? ` -l ${starModels[0].limitValue}` : ''}`
+                                + `${starModels[0].limit !== 'none' ? ` -n ${starModels[0].measurements}` : ''}`
+                                + `${starModels[0].readingsIgnored ? ' -d 0' : ''}`
+                        console.log('cmd for model:', cmd);
+                        break;      
+        
                     default:
                         break;
                 }
+                exec(cmd, (err, stdout, stderr) => {
+                    if(err){
+                        console.log('error:', err);
+                        return;
+                    } else {
+                        console.log('output:', stdout);
+                        console.log('std error:', stderr);
+                    };
+                });
                  
 
             }else if (starModels.length > 1){
