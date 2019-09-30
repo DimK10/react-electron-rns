fe63427908747002de62f6902eca583e96aa0afd
DimK10 <james_k23@hotmail.gr>
2019-09-30 20:28:42 +0300

    Added a basic loading render when data are being proccessed in the server

--- src/components/StarDashboardPage.js
+++ src/components/StarDashboardPage.js
@@ -5,6 +5,7 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import '../styles/StarDashBoardPage.css';
 
 
 
@@ -22,23 +23,57 @@ const useStyles = makeStyles(theme => ({
       }
   }));
 
-const StarDashboardPage = () => {
-    
-    const classes = useStyles();
-    
-    return(
-        <div>
-            <div className={classes.links}>
-                <NavLink to="/create" activeClassName="is-active" className={classes.navlink}>
-                    <Fab variant="extended" color="primary" aria-label="add">
-                        <AddIcon /> 
-                        <Typography> Add A Model</Typography>
-                    </Fab>
-                </NavLink>
+class StarDashboardPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: false
+        };
+    };
+    updateLoading = (loading) => {
+        this.setState({ loading  });
+    };
+
+    // classes = useStyles();
+
+    render() {
+        return(
+            <div>
+                <div className="links"/*className={this.classes.links}*/>
+                   {
+                       !this.state.loading
+                       &&
+                        <NavLink to="/create" activeClassName="is-active" className="navlink"/*className={this.classes.navlink}*/>
+                            <Fab variant="extended" color="primary" aria-label="add">
+                                <AddIcon /> 
+                                <Typography> Add A Model</Typography>
+                            </Fab>
+                        </NavLink>
+                   }
+                </div>
+            <StarList parentCallback = {this.updateLoading} />
             </div>
-            <StarList />
-        </div>
-    );
-};
+        );
+    }
+  };
+
+// const StarDashboardPage = () => {
+    
+//     const classes = useStyles();
+    
+//     return(
+//         <div>
+//             <div className={classes.links}>
+//                 <NavLink to="/create" activeClassName="is-active" className={classes.navlink}>
+//                     <Fab variant="extended" color="primary" aria-label="add">
+//                         <AddIcon /> 
+//                         <Typography> Add A Model</Typography>
+//                     </Fab>
+//                 </NavLink>
+//             </div>
+//             <StarList />
+//         </div>
+//     );
+// };
 
 export default StarDashboardPage;
--- src/components/StarList.js
+++ src/components/StarList.js
@@ -3,65 +3,154 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import StarListItem from './StarListItem';
 import selectStars from '../selectors/stars';
-import { sendModelsData, addChartData } from '../actions/stars.js';
+import { sendModelsData } from '../actions/stars.js';
+import uuid from 'uuid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import uuid from 'uuid';
+import { BallBeat } from 'react-pure-loaders';
 import '../styles/StarList.css'
 
-const StarList = (props) => {
-    
-    // saveDataToDb(props.stars);
 
-    const postModels = () => {
+export class StarList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: false
+        };
+    };
+
+    postModels = () => {
 
         (async () => {
             let stars = {
                 id: uuid(),
-                starModels: [...props.stars].reverse() // For some reason, stars are saved in reverse order even with push
+                starModels: [...this.props.stars].reverse() // For some reason, stars are saved in reverse order even with push
             };
             console.log('stars data:', stars);
-            
+
+            this.props.parentCallback(true);
+            this.setState({ loading: true });
+
             let data = await sendModelsData(stars);
             console.log('data send from server:', data);
 
             data.models.forEach((element, index) => {
                 if(element !== 'succeeded') {
-                    alert(`${props.stars[index].starName} has failed. Reason ${element.error}`);
+                    alert(`${this.props.stars[index].starName} has failed. Reason ${element.error}`);
                 }
             });
 
-            props.history.push('/chart', { chartData: data.valuesForGraph })
+            this.setState({ loading: false });
+
+            this.props.history.push('/chart', { chartData: data.valuesForGraph })
         })();
         
     };
 
-    return (
-        <div className="container">
-            {
-                props.stars.length !== 0 
-                ? (
-                    <Typography variant="h4">Star Models List</Typography>
-                ) : (
-                    <Typography variant="h4">Add Star Models to Start...</Typography>
-                )
-            }
-            {
-                [...props.stars].reverse().map((star) => {
-                    return <StarListItem key={star.id} star={star} />
-                })
-            }
-            {
-                props.stars.length !== 0
-                &&
-                <Button variant="contained" onClick={postModels}>
-                    <Typography>Create Graph</Typography>
-                </Button>
-            }
-        </div>
-    )
 
-};
+    render() {
+        return (
+            <div className="container">
+                {
+                    this.props.stars.length !== 0 && !this.state.loading
+                    ? (
+                        <Typography variant="h4">Star Models List</Typography>
+                    ) : (
+                        !this.state.loading 
+                        ?
+                        (
+                            <Typography variant="h4">Add Star Models to Start...</Typography>
+                        ) 
+                        : 
+                        (
+                            <Typography variant="h4">Loading Graph. Please be patient!</Typography>
+                        )
+                    )
+                }
+                {
+                    !this.state.loading
+                    &&
+                    this.props.stars.map((star) => {
+                        return <StarListItem key={star.id} star={star} />
+                    })
+                }
+                {
+                    this.props.stars.length !== 0 
+                    && 
+                    !this.state.loading
+                    &&
+                    <Button variant="contained" onClick={this.postModels}>
+                        <Typography>Create Graph</Typography>
+                    </Button>
+                }
+                {
+                    this.state.loading
+                    &&
+                    <BallBeat
+                        color={'#123abc'}
+                        loading={this.state.loading}
+                    />
+                }
+            </div>
+        );
+    }
+}
+
+
+
+// const StarList = (props) => {
+    
+//     // saveDataToDb(props.stars);
+
+//     const postModels = () => {
+
+//         (async () => {
+//             let stars = {
+//                 id: uuid(),
+//                 starModels: [...props.stars].reverse() // For some reason, stars are saved in reverse order even with push
+//             };
+//             console.log('stars data:', stars);
+            
+//             let data = await sendModelsData(stars);
+//             console.log('data send from server:', data);
+
+//             data.models.forEach((element, index) => {
+//                 if(element !== 'succeeded') {
+//                     alert(`${props.stars[index].starName} has failed. Reason ${element.error}`);
+//                 }
+//             });
+
+//             props.history.push('/chart', { chartData: data.valuesForGraph })
+//         })();
+        
+//     };
+
+//     return (
+//         <div className="container">
+//             {
+//                 props.stars.length !== 0 
+//                 ? (
+//                     <Typography variant="h4">Star Models List</Typography>
+//                 ) : (
+//                     <Typography variant="h4">Add Star Models to Start...</Typography>
+//                 )
+//             }
+//             {
+//                 [...props.stars].reverse().map((star) => {
+//                     return <StarListItem key={star.id} star={star} />
+//                 })
+//             }
+//             {
+//                 props.stars.length !== 0
+//                 &&
+//                 <Button variant="contained" onClick={postModels}>
+//                     <Typography>Create Graph</Typography>
+//                 </Button>
+//             }
+//         </div>
+//     )
+
+// };
 
 const mapStateToProps = (state) => {
     return {
