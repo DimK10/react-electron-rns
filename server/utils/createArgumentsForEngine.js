359850f6f85d16251f48b5be96686495e2c77c6a
DimK10 <james_k23@hotmail.gr>
2019-10-02 21:23:36 +0300

    Added all the models for cmd to rns executable

--- server/utils/createArgumentsForEngine.js
+++ server/utils/createArgumentsForEngine.js
@@ -9,7 +9,8 @@ const createArgumentsForEngine = (cmd, starModel) => {
                     + ` -r ${starModel.valueForSecondInput}`
                     + `${starModel.limit == 'limitSecondValue' ? ` -l ${starModel.limitValue}` : ''}`
                     + `${starModel.limit !== 'none' ? ` -n ${starModel.measurements}` : ''}`
-                    + `${starModel.readingsIgnored ? ' -d 0' : ''}`
+                    // + `${starModel.readingsIgnored ? ' -d 0' : ''}` This is not possible due to regex catching all the uneccecary values -- removing
+                    + ' -d 0'
                     
             console.log('cmd for model:', cmd);
             return cmd;
@@ -20,11 +21,68 @@ const createArgumentsForEngine = (cmd, starModel) => {
                     + ` -m ${starModel.valueForSecondInput}`
                     + `${starModel.limit == 'limitSecondValue' ? ` -l ${starModel.limitValue}` : ''}`
                     + `${starModel.limit !== 'none' ? ` -n ${starModel.measurements}` : ''}`
-                    + `${starModel.readingsIgnored ? ' -d 0' : ''}`
+                    // + `${starModel.readingsIgnored ? ' -d 0' : ''}`
+                    + ' -d 0'
             console.log('cmd for model:', cmd);
-            return cmd;    
+            return cmd;
+        case 'rmass':
+            cmd += ` -e ${starModel.centralEnergyDensity}`
+            + `${starModel.limit === 'limitEnergy' ? ` -l ${starModel.limitValue}` : ''}`
+            + ` -z ${starModel.valueForSecondInput}`
+            + `${starModel.limit === 'limitSecondValue' ? ` -l ${starModel.limitValue}` : ''}`
+            + `${starModel.limit !== 'none' ? ` -n ${starModel.measurements}` : ''}`
+            + ' -d 0'
 
-        default:
+            console.log('cmd for model:', cmd);
+            return cmd;
+        case 'omega':
+            cmd += ` -e ${starModel.centralEnergyDensity}`
+            + `${starModel.limit === 'limitEnergy' ? ` -l ${starModel.limitValue}` : ''}`
+            + ` -o ${starModel.valueForSecondInput}`
+            + `${starModel.limit === 'limitSecondValue' ? ` -l ${starModel.limitValue}` : ''}`
+            + `${starModel.limit !== 'none' ? ` -n ${starModel.measurements}` : ''}`
+            + ' -d 0'
+            
+            console.log('cmd for model:', cmd);
+            return cmd;
+
+        case 'jmoment':
+            cmd += ` -e ${starModel.centralEnergyDensity}`
+            + `${starModel.limit === 'limitEnergy' ? ` -l ${starModel.limitValue}` : ''}`
+            + ` -j ${starModel.valueForSecondInput}`
+            + `${starModel.limit === 'limitSecondValue' ? ` -l ${starModel.limitValue}` : ''}`
+            + `${starModel.limit !== 'none' ? ` -n ${starModel.measurements}` : ''}`
+            + ' -d 0'
+            
+            console.log('cmd for model:', cmd);
+            return cmd;
+
+        case 'static':
+            cmd += ` -e ${starModel.centralEnergyDensity}`
+            + `${starModel.limit === 'limitEnergy' ? ` -l ${starModel.limitValue}` : ''}`
+            + `${starModel.limit !== 'none' ? ` -n ${starModel.measurements}` : ''}`
+            + ' -d 0'
+            
+            console.log('cmd for model:', cmd);
+            return cmd;
+
+        case 'kepler':
+            cmd += ` -e ${starModel.centralEnergyDensity}`
+            + `${starModel.limit === 'limitEnergy' ? ` -l ${starModel.limitValue}` : ''}`
+            + `${starModel.labelForSecondInput === 'Tolerance' ? ` -b ${starModel.valueForSecondInput}` : ''}`
+            + `${starModel.limit !== 'none' ? ` -n ${starModel.measurements}` : ''}`
+            + ' -d 0'
+            
+            console.log('cmd for model:', cmd);
+            return cmd;
+
+        case 'test':
+            // Nothing additional needs to be passed for test model, except the -d 0 flag for not useful values
+            cmd+= ' -d 0';
+            console.log('cmd for model:', cmd);
+            return cmd;
+
+            default:
             break;
     }
 }
